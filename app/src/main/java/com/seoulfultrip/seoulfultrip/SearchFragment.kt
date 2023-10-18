9e8022f88eb1a3741bc0891d56c4b81b53b814fe
20210699 <20210699@duksung.ac.kr>
2023-10-18 13:13:52 +0900

    search page top bar

--- app/src/main/java/com/seoulfultrip/seoulfultrip/SearchFragment.kt
+++ app/src/main/java/com/seoulfultrip/seoulfultrip/SearchFragment.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -120,15 +121,17 @@ class SearchFragment : Fragment() {
         binding.btnSearch.setOnClickListener{
 
             var keyword = binding.edtProduct.text.toString()
-           val intent = Intent(getActivity(), SearchResultActivity::class.java)
-            intent.putExtra("keyword", keyword)
-            binding.edtProduct.text.clear()
-            startActivity(intent)
+            if(keyword.isNullOrBlank()){
+                Toast.makeText(this.context,"검색어를 입력해주세요", Toast.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(getActivity(), SearchResultActivity::class.java)
+                intent.putExtra("keyword", keyword)
+                binding.edtProduct.text.clear()
+                startActivity(intent)
+            }
+
         }
-
         return binding.root
-
-
        // return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
