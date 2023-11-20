a9e446626149f1d54f8d5a2af948c5b6117a52a0
kangsujung <rkdtnwjd1030@naver.com>
2023-11-20 19:17:13 +0900

    도착지 설정 레이아웃 구현

--- app/src/main/java/com/seoulfultrip/seoulfultrip/StartplaceActivity.kt
+++ app/src/main/java/com/seoulfultrip/seoulfultrip/StartplaceActivity.kt
@@ -4,17 +4,21 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.view.ViewGroup
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.seoulfultrip.seoulfultrip.databinding.ActivityStartplaceBinding
+import com.seoulfultrip.seoulfultrip.databinding.ItemSaveBinding
 import kotlin.math.log
 
 class StartplaceActivity : AppCompatActivity() {
     val itemList = mutableListOf<PlaceStorage>()
     var pname: Array<out String>?= arrayOf("")
+    private val adapter = MySelectAdapter(this, itemList)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityStartplaceBinding.inflate(layoutInflater)
@@ -25,12 +29,13 @@ class StartplaceActivity : AppCompatActivity() {
         getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
 
         binding.saveRecyclerView.layoutManager = LinearLayoutManager(this)
-        binding.saveRecyclerView.adapter = MySaveAdapter(this, itemList)
+        //binding.saveRecyclerView.adapter = StartplaceAdapter(this, pname)
 
         //intent.getParcelableArrayListExtra()
         pname = intent.getStringArrayExtra("a")
         Log.d("장소이름", "${pname}")
 
+        adapter.savepname
 
     }
 
@@ -53,3 +58,23 @@ class StartplaceActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 }
+
+/*
+class StartplaceViewHolder(val binding: ItemSaveBinding):  RecyclerView.ViewHolder(binding.root)
+class StartplaceAdapter(val context: Context, val datas: Array<out String>?): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    val itemList = mutableListOf<PlaceStorage>()
+    private val adapter = MySelectAdapter(context, itemList)
+    override fun getItemCount(): Int {
+        return datas?.size?: 0
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder
+            =StartplaceViewHolder(ItemSaveBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding=(holder as StartplaceViewHolder).binding
+        binding.itemNameView.text = adapter.savepname.iterator().next()
+
+    }
+}*/
