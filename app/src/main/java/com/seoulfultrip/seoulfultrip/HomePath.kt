9922d37f7695bf889fb83c1e5967e5cbae7e5eb9
20210699 <20210699@duksung.ac.kr>
2023-11-22 19:29:48 +0900

    Dijkstra Algorithm

--- app/src/main/java/com/seoulfultrip/seoulfultrip/HomePath.kt
+++
@@ -1,9 +0,0 @@
-package com.seoulfultrip.seoulfultrip
-
-data class HomePath(
-    val pathName: String,   // 경로 이름
-    val date: String,       // 경로를 만든 날짜
-    val start: String,      // 시작 장소 이름
-    val end: String,        // 마지막 장소 이름
-    val imageResource: Int  // 이미지 리소스 ID (정수)를 저장할 필드
-)
